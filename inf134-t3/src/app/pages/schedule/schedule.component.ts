22981edf89d4781ec251e0ac3418972dedd31301
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-05-25 00:23:23 -0700

    add north campus image map

--- inf134-t3/src/app/pages/schedule/schedule.component.ts
+++ inf134-t3/src/app/pages/schedule/schedule.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.css'],
 })
 export class ScheduleComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  selectArea(event) {
+    var target = event.target || event.srcElement || event.currentTarget;
+    var idAttr = target.attributes.title;
+    var value = idAttr.nodeValue;
+    console.log(value);
   }
-
 }
