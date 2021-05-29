d1ee1e1ed6980471f6df72f9d061c5eb62290031
Cindy Tang <cindyt821@gmail.com>
2021-05-29 12:03:44 -0700

    finished!!!

--- inf134-t3/src/app/components/organizations-list/organizations-list.component.ts
+++ inf134-t3/src/app/components/organizations-list/organizations-list.component.ts
@@ -10,17 +10,23 @@ import { Router } from '@angular/router';
 export class OrganizationsListComponent implements OnInit {
   public organizations;
   public selectedOrg;
-  public orgsEvent
+  public orgsEvent;
+  public collapseStatusOrgs = [];
+  public keyword: string;
 
   constructor(public dataService: DataService, public router: Router) {}
 
   ngOnInit(): void {
     this.organizations = this.dataService.getOrganizations();
+    for  (let org of this.organizations) {
+      this.collapseStatusOrgs.push(false);
+    }
   }
 
-  public selectOrganization(org) {
+  public selectOrganization(org, index) {
     this.selectedOrg = org;
     this.getEventDescription(org.id);
+    this.toggleDescription(index);
   }
 
   private getEventDescription(org_id) {
@@ -31,4 +37,28 @@ export class OrganizationsListComponent implements OnInit {
     // TODO: goto the actual event page
     this.router.navigate(['/event/' + event_id]);
   }
+
+  public sortByName() {
+    this.organizations = this.dataService.getOrganizations();
+    console.log(this.organizations);
+    this.organizations.sort(function compare(kv1, kv2) {
+        return kv1['name'].localeCompare(kv2['name']);
+      });
+  }
+
+  public toggleDescription(i) {
+    this.collapseStatusOrgs[i] = !this.collapseStatusOrgs[i];
+  }
+
+  public searchForOrg() {
+    console.log(this.keyword);
+    let searchResultOrgs = []
+    for (let org of this.organizations) {
+      if (org.name.toLowerCase().includes(this.keyword) || org.description.toLowerCase().includes(this.keyword)) {
+        searchResultOrgs.push(org);
+      }
+    }
+    this.organizations = searchResultOrgs;
+  }
+
 }
