a87b54939c66f1b7fe2a8ae2d23539c46261f226
Iqra Abbaali <iqra.abbaali@gmail.com>
2021-05-30 02:43:53 -0700

    implement toggle schedule logic

--- inf134-t3/src/app/pages/saved-events/saved-events.component.ts
+++ inf134-t3/src/app/pages/saved-events/saved-events.component.ts
@@ -15,11 +15,13 @@ export class SavedEventsComponent implements OnInit {
   savedEvents = {};
   filterBySchedule = false;
   displayedEvents = [];
+  schedule = [];
 
   constructor(private router: Router, private dataservice: DataService) {}
 
   ngOnInit(): void {
     let temp = this.dataservice.getSavedEvents();
+    this.schedule = this.dataservice.getSchedule();
     for (let [key, value] of Object.entries(temp)) {
       this.savedEvents[key] = [];
       for (let event of value) {
@@ -30,6 +32,8 @@ export class SavedEventsComponent implements OnInit {
     }
     this.displayedEvents = this.savedEvents['All'];
     this.categories = this.dataservice.getCategories();
+
+    console.log(this.schedule);
   }
 
   toggleSchedule() {
@@ -57,9 +61,34 @@ export class SavedEventsComponent implements OnInit {
   }
 
   isEventAlignWithSchedule(event) {
-    if (event.id > 8) {
-      return false;
+    for (let [building, value] of Object.entries(this.schedule)) {
+      if (event.location.includes(building)) {
+        let date = new Date(event.date);
+        let eventDay = new Intl.DateTimeFormat('en-US', {
+          weekday: 'long',
+        }).format(date);
+        for (let options of value) {
+          let pieces1 = options[1].split(':');
+          let startHr = parseInt(pieces1[0]);
+          let startMin = parseInt(pieces1[1]);
+          let startTime = startHr * 60 + startMin;
+
+          let pieces2 = options[2].split(':');
+          let endHr = parseInt(pieces2[0]);
+          let endMin = parseInt(pieces2[1]);
+          let endTime = endHr * 60 + endMin;
+
+          for (let day of options[0]) {
+            if (day === eventDay) {
+              let eventTime = date.getHours() * 60 + date.getMinutes();
+              if (eventTime >= startTime - 30 && eventTime <= endTime + 30) {
+                return true;
+              }
+            }
+          }
+        }
+      }
     }
-    return true;
+    return false;
   }
 }
